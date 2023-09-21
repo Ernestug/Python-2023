92dbd4b918ec9c3ce4314adadb548e3810a36874
Ernestug <ernestugwoke@gmail.com>
2023-09-21 17:33:13 +0200

    changed the operator from == to !=

--- lesson_2.py
+++ lesson_2.py
@@ -3,5 +3,5 @@ a = 5
 b = 3
 c = 10
 ## compute and print the logical output
-print(a + b *c ==80) # this should produce True
-print(a / b + c == 5 / 13) ## This should produce True
+print(a + b *c != 80) # this should produce True
+print(a / b + c != 5 / 13) ## This should produce True
