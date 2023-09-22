2285e3d46d31475da9838b0b2cc3a9a326464510
Ernestug <ernestugwoke@gmail.com>
2023-09-22 16:41:57 +0200

    formated version of the lesson_2.py

--- lesson_2.py
+++ lesson_2.py
@@ -1,7 +1,7 @@
-## define variables
-a = 5
-b = 3
-c = 10
-## compute and print the logical output
-print(a + b *c ==80) # this should produce True
-print(a / b + c == 5 / 13) ## This should produce True
+"""define variables"""
+A = 5
+B = 3
+C = 10
+# compute and print the logical output
+print(A + B *C !=80) # this should produce True
+print(A / B + C != C / 13) # This should produce True
